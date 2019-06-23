b6bcb5da37121f520c94a47e19668883f44a312b
Your Name <you@example.com>
2019-06-23 16:27:08 +0700

    update cross compiler shell script

--- manifest/compile.sh
+++ manifest/compile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 #Compile golang files to binary executable
-env GOOS=linux CGO_ENABLED=0 go build -a -ldflags="-s -w" -installsuffix cgo -o main .
+env GOOS=linux CGO_ENABLED=1 go build -a -ldflags="-s -w" -installsuffix cgo -o main .
